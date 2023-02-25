7fbaa1174992e90290c7cdc9510d252bfcefe72f
User <User@DESKTOP-MANCTJR>
2023-02-25 18:51:00 +0700

    Добавьте файлы проекта.

---
+++ Car.cs
@@ -0,0 +1,20 @@
+﻿namespace Garage
+{
+    class Car
+    {
+        public string brand;
+        public string model;
+        public string color;
+        public string number;
+
+        public Car(string inputBrand = "Неизвестно", string inputModel = "Неизвестно", string inputColor = "Неизвестно", string inputNumber = "Неизвестно")
+        {
+            brand = inputBrand;
+            model = inputModel;
+            color = inputColor;
+            number = inputNumber;
+        }
+
+        public override string ToString() => $"Марка: {brand}\nМодель: {model}\nЦвет: {color}\nНомер: {number}";   
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Garage;
+
+class Program
+{
+
+    static void Main()
+    {
+        List<Car> carGarage = new();
+        bool flag = true;
+
+        while (flag)
+        {
+            Console.WriteLine("Добавить автомобиль - 1\nВывод всех автомобилей - 2\nВыход - 3");
+            int code = int.Parse(Console.ReadLine());
+            switch (code)
+            {
+                case 1:
+                    Console.WriteLine("Введите марку, модель, цвет и номер: ");
+                    string brand = Console.ReadLine();
+                    string model = Console.ReadLine();
+                    string color = Console.ReadLine();
+                    string number = Console.ReadLine();
+                    carGarage.Add(new Car(brand, model, color, number));                  
+                    break;
+
+                case 2:
+                    foreach (var car in carGarage)
+                    {
+                        Console.WriteLine(car);
+                        Console.WriteLine();
+                    }
+                    break;
+
+                case 3:
+                    Console.WriteLine("Выход.");
+                    flag = false;
+                    break;
+            }
+        }
+    }
+}
